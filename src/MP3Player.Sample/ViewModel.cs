b15ac3e6c3169f9bf0be667983c82e1703f5a139
Behzad <behzad.khosravifar@gmail.com>
2021-03-29 00:40:45 +0430

    Fixed some bindings of sample UI

--- src/MP3Player.Sample/ViewModel.cs
+++ src/MP3Player.Sample/ViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel;
+using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace MP3Player.Sample
 {
     public class ViewModel : INotifyPropertyChanged
     {
+        private IWavePlayer _player;
         public event PropertyChangedEventHandler PropertyChanged;
         public double Position { get; set; }
         public int Volume { get; set; }
@@ -12,5 +14,38 @@ namespace MP3Player.Sample
         public ICommand PlayPauseCommand { get; set; }
         public ICommand NextCommand { get; set; }
         public ICommand PreviousCommand { get; set; }
+
+        public ViewModel()
+        {
+            PlayPauseCommand = new RelayCommandAsync(OnPlayPause);
+        }
+
+        private Task OnPlayPause()
+        {
+            return IsPlaying ? Pause() : Play();
+        }
+
+        private Task Pause()
+        {
+            IsPlaying = false;
+            return Task.CompletedTask;
+        }
+
+        private Task Play()
+        {
+            IsPlaying = true;
+            return Task.CompletedTask;
+        }
+
+        private void OnPositionChanged()
+        {
+
+        }
+
+        private void OnVolumeChanged()
+        {
+
+        }
+
     }
 }
